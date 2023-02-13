650b68445432de70ade76b92a80d04387cc4e12a
infoomics <sbx_94@hotmail.com>
2023-02-13 17:26:06 +0000

    complete task 9

--- models/rides.js
+++ models/rides.js
@@ -1,1 +1,16 @@
-exports.selectRidesByParkId = () => {};
+const db = require('../db/connection');
+const format = require('pg-format');
+
+exports.selectRidesByParkId = (park_id) => {
+    const selectRidesByParkIdStr = format(
+        `SELECT * 
+        FROM rides 
+        LEFT OUTER JOIN parks 
+        ON rides.park_id = parks.park_id
+        WHERE rides.park_id = %L;`,
+        [park_id]
+    );
+
+    return db.query(selectRidesByParkIdStr)
+    .then(selectParksResult => selectParksResult.rows);
+};
